081f40521872ae4ea1836bdb7def1a4a477ac3ba
Mapala1965 <mapalamegha@gmail.com>
2021-09-22 13:29:07 -0400

    second commit

--- src/projectgitdemo20/DDArray.java
+++ src/projectgitdemo20/DDArray.java
@@ -5,12 +5,28 @@ public class DDArray {
 	public static void main(String[] args) {
 		
 		int[][] values = {{1,2,3,},{4,5,6},{7,8,9}};
+		
 		for(int i=0; i< values.length; i++) {
 			for(int j=0; j<values[i].length; j++) {
 				System.out.print(values[i][j] + "\t");
 			}
 			System.out.println("");
 		}
+		
+		int[][] contents = {{11,22,33,},{44,55,66},{77,88,99}};
+		
+		System.out.println("                              ");
+		System.out.println("Loop using Enhanced for loop: ");
+		for (int[] eachRow : contents) {
+			for (int j : eachRow) {
+				System.out.print(j + "\t");
+			}
+			System.out.println("");
+		}
+		
+
+		
+		
 
 	}
 
